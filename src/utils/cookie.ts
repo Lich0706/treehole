a1bdf505b961fd1dc0b4a557a46d23b80151dc0d
李晨阳 <lich@azaleas-air.lan>
2024-05-30 17:32:49 -0400

    sign up

---
+++ src/utils/cookie.ts
@@ -0,0 +1,18 @@
+import Cookies from 'universal-cookie';
+
+export const setCookies = (cookiesToSet: { [key: string]: string }) => {
+  const cookies = new Cookies();
+  Object.keys(cookiesToSet).forEach(function (key) {
+    cookies.set(key, cookiesToSet[key], { path: '/', maxAge: 60 * 60 * 24 * 30 });
+  });
+};
+
+export const getCookie = (key: string) => {
+  const cookies = new Cookies();
+  return cookies.get(key);
+};
+
+export function isSignedIn() {
+  const cookies = new Cookies();
+  return cookies.get('token') !== undefined;
+}
